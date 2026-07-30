c234466f096e8dbea30a41d3f6cbb49915c419da
kirti-2003 <kirti.anand903@gmail.com>
2026-07-30 15:05:35 +0530

    initial commit

---
+++ config/apiClient.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  timeout: 30000,
+});
+
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const message =
+      error.response?.data?.detail ||
+      error.response?.data?.message ||
+      error.message ||
+      "Something went wrong.";
+
+    return Promise.reject(new Error(message));
+  }
+);
+
+export default apiClient;
